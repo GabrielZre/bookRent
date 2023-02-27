597b378e58190393bd77180d0fca61fb0990ca28
GabrielZre <104325153+GabrielZre@users.noreply.github.com>
2023-02-27 22:49:31 +0100

    Updated sql file

--- bookrent.sql
+++ bookrent.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 26 Lut 2023, 17:07
+-- Czas generowania: 27 Lut 2023, 22:47
 -- Wersja serwera: 10.4.22-MariaDB
 -- Wersja PHP: 8.1.2
 
@@ -40,7 +40,10 @@ CREATE TABLE `tbook` (
 
 INSERT INTO `tbook` (`id`, `title`, `author`, `isbn`) VALUES
 (2, 'Hobbit', 'Tolkiens', '8592003857123'),
-(3, 'Power of Rings', 'Tolkiens', '6285921923515');
+(3, 'Power of Rings', 'Tolkiens', '6285921923515'),
+(5, 'Uphill Down', 'Megan Holly', '3789874510049'),
+(6, 'Peppa', 'Baba Jaga', '1959021004291'),
+(7, 'Psychologia Ukladow', 'J.K Maxil', '8293219002031');
 
 -- --------------------------------------------------------
 
@@ -54,17 +57,22 @@ CREATE TABLE `tloan` (
   `end_date` date DEFAULT NULL,
   `book_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `loan_is_active` tinyint(1) NOT NULL
+  `loan_is_active` tinyint(1) NOT NULL,
+  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `surname` varchar(50) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Zrzut danych tabeli `tloan`
 --
 
-INSERT INTO `tloan` (`id`, `start_date`, `end_date`, `book_id`, `user_id`, `loan_is_active`) VALUES
-(4, '2023-02-09', '2023-02-27', 2, 1, 0),
-(5, '2023-02-10', '2023-02-12', 3, 2, 1),
-(13, '2023-02-26', '2023-03-12', 2, 2, 1);
+INSERT INTO `tloan` (`id`, `start_date`, `end_date`, `book_id`, `user_id`, `loan_is_active`, `name`, `surname`) VALUES
+(16, '2023-02-27', '2023-03-13', 2, 2, 1, 'Gabriel', 'Zrebiec'),
+(17, '2023-02-27', '2023-03-13', 3, 2, 1, 'Michal', 'Zrebiec'),
+(18, '2023-02-10', '2023-02-24', 5, 2, 1, 'Mateusz', 'Martyniuk'),
+(20, '2023-02-27', '2023-03-13', 6, 3, 0, 'Gabriel', 'Zrebiecowaty'),
+(21, '2023-02-27', '2023-03-13', 7, 3, 0, 'Tomasz', 'Pasterniak'),
+(22, '2023-02-27', '2023-03-13', 6, 3, 0, 'Gabriel', 'Prawdziwy');
 
 -- --------------------------------------------------------
 
@@ -84,8 +92,9 @@ CREATE TABLE `tuser` (
 --
 
 INSERT INTO `tuser` (`id`, `login`, `password`, `role`) VALUES
-(1, 'janusz', '2e3c5df4b876dfde356c18708a79d6c7', 'ADMIN'),
-(2, 'admin', '1671dfc274ad7e8d560df28ac0eb3e45', 'ADMIN');
+(2, 'admin', '1671dfc274ad7e8d560df28ac0eb3e45', 'ADMIN'),
+(3, 'gabriel', '4754793fa06ffec9db217a94cc2215c3', 'USER'),
+(4, 'adam', 'fc4a1414afc2d554746f62d6772cb4b2', 'USER');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -103,7 +112,6 @@ ALTER TABLE `tbook`
 ALTER TABLE `tloan`
   ADD PRIMARY KEY (`id`),
   ADD KEY `book_id` (`book_id`),
-  ADD KEY `book_id_2` (`book_id`),
   ADD KEY `user_id` (`user_id`);
 
 --
@@ -121,19 +129,19 @@ ALTER TABLE `tuser`
 -- AUTO_INCREMENT dla tabeli `tbook`
 --
 ALTER TABLE `tbook`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT dla tabeli `tloan`
 --
 ALTER TABLE `tloan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT dla tabeli `tuser`
 --
 ALTER TABLE `tuser`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Ograniczenia dla zrzutów tabel
